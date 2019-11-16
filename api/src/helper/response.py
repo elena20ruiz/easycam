e83a8773af248075b7759b743d8baa55796df0e1
Albert Suarez <alsumo95@gmail.com>
2019-11-16 15:08:12 +0100

    Added response helper file

---
+++ api/src/helper/response.py
@@ -0,0 +1,13 @@
+def make(error, message=None, response=None):
+    response_dict = dict(error=error)
+    if error:
+        assert type(message) is str
+        response_dict['message'] = message
+    else:
+        assert type(response) is dict
+        response_dict['response'] = response
+    return response_dict, 200
+
+
+def get(attribute_name, json_response, default=None):
+    return default if attribute_name not in json_response else json_response[attribute_name]
