f35425351e1ec95142248407265d136f5031648d
Albert Suarez <alsumo95@gmail.com>
2019-11-16 15:36:33 +0100

    Implemented input validation for /download

---
+++ api/src/__init__.py
@@ -0,0 +1,10 @@
+MESSAGE_OK = 'OK'
+MESSAGE_ERROR = 'Unexpected error while processing the API'
+MESSAGE_ERROR_PARAMETERS = 'Not all input parameters were specified correctly.'
+
+
+__all__ = [
+    'MESSAGE_OK',
+    'MESSAGE_ERROR',
+    'MESSAGE_ERROR_PARAMETERS'
+]
--- api/src/api/image.py
+++ api/src/api/image.py
@@ -1,5 +1,27 @@
+from flask import request
+
+from src import *
+from src.helper import response, log
+from src.helper.timer import Timer
+
+
 def download():
-    return 'OK'
+    try:
+        with Timer('Validate input'):
+            request_json = request.json
+            batch_id = response.get('batch_id', request_json)
+            image_base64 = response.get('image_base64', request_json)
+            image_id = response.get('image_id', request_json)
+            if not all([batch_id, image_base64, image_id]):
+                log.error(f'Error while getting parameters in {download.__name__} function: {request_json}')
+                return response.make(error=True, message=MESSAGE_ERROR_PARAMETERS)
+
+        return response.make(error=False, response=MESSAGE_OK)
+
+    except Exception as e:
+        log.error(f'Exception while processing {download.__name__} function: [{e}]')
+        log.exception(e)
+        return response.make(error=True, message=MESSAGE_ERROR)
 
 
 def cluster(batch_id):
